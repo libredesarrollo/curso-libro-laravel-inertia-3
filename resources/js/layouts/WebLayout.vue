a7878760c108a0ed19fb7156c57bd0df6f9a760a
Andres Cruz <desarrollolibre.net@gmail.com>
2026-04-15 11:59:56 -0400

    transiciones

--- resources/js/layouts/WebLayout.vue
+++ resources/js/layouts/WebLayout.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import CartCount from '@/fragment/CartCount.vue';
 import ToastContainer from '@/components/ToastContainer.vue';
+import { useCurrentUrl } from '@/composables/useCurrentUrl';
+
 </script>
 
 <template>
-    <div class="px-4 py-6">
-        <slot />
+    <div>
+        <div class="px-4 py-6">
+            <slot />
+        </div>
+        <CartCount />
+        <ToastContainer />
     </div>
-    <CartCount />
-    <ToastContainer />
 </template>
